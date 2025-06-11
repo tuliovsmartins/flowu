bd09fec2afaa0e3f03337382b52fdda39a469d8b
delaney <delaneymaciel@hotmail.com>
2025-06-10 21:13:24 -0300

    Criação da classe FlowNode

---
+++ src/main/java/com/flowu/model/FlowNode.java
@@ -0,0 +1,45 @@
+package com.flowu.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "flow_nodes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class FlowNode {
+
+    @Id
+    private String id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name ="flow_id", nullable = false)
+    private Flow flow;
+
+    private String type;
+
+    @Column(columnDefinition = "JSONB")
+    private String position;
+
+    @Column(columnDefinition = "JSONB")
+    private String data;
+
+    private Integer width;
+    private Integer height;
+    private Boolean selected;
+    private Boolean dragging;
+
+    @Column(name = "position_absolute", columnDefinition = "JSONB")
+    private String positionAbsolute;
+
+    @Column(name = "executed_success_count")
+    private Long executedSuccessCount = 0L;
+
+    @Column(name = "executed_error_count")
+    private Long executedErrorCount = 0L;
+
+}
